6a623079b7d29e50780f390568b37a54dff7e34d
Lemsito22 <lemquilca22@gmail.com>
2026-05-11 12:50:16 +0200

    Terminado

--- Memory_JavaFX/src/main/java/org/example/memory_javafx/HelloController.java
+++ Memory_JavaFX/src/main/java/org/example/memory_javafx/HelloController.java
@@ -1,10 +1,12 @@
 package org.example.memory_javafx;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 import org.example.memory_javafx.Model.Carta;
 import org.example.memory_javafx.Model.Tablero;
 
@@ -15,6 +17,7 @@ public class HelloController {
     private Button primerBotonSeleccionado;
     private Carta primeraCartaSeleccionada;
     private boolean bloqueado = false;
+    private int parejasEncontradas = 0;
 
     @FXML
     public void initialize(){
@@ -34,7 +37,54 @@ public class HelloController {
 
             boton.setOnAction(e -> manejarClick(boton, indice));
 
-            gridTablero.add(boton, i%4,1/4);
+            gridTablero.add(boton, i % 4,i / 4);
         }
+    }
+
+    private void manejarClick(Button botonActual, int indice){
+        Carta cartaActual = tablero.getCarta(indice);
+
+        if (bloqueado || cartaActual.isEmparejada() || botonActual == primerBotonSeleccionado){
+            return;
+        }
+
+        botonActual.setText(String.valueOf(cartaActual.getId()));
+
+        if (primeraCartaSeleccionada == null ){
+            primeraCartaSeleccionada = cartaActual;
+            primerBotonSeleccionado = botonActual;
+        } else {
+            if (cartaActual.getId() == primeraCartaSeleccionada.getId()){
+                cartaActual.setEmparejada(true);
+                primeraCartaSeleccionada.setEmparejada(true);
+                parejasEncontradas++;
+                limpiarSeleccion();
+
+                if (parejasEncontradas == 8 ){
+                    finalizarJuego();
+                }
+            } else {
+                bloqueado=true;
+                PauseTransition pausa = new PauseTransition(Duration.seconds(1));
+                pausa.setOnFinished(e -> {
+                    botonActual.setText("?");
+                    primerBotonSeleccionado.setText("?");
+                    limpiarSeleccion();
+                    bloqueado=false;
+                });
+                pausa.play();
+            }
+        }
+    }
+    private void limpiarSeleccion(){
+        primeraCartaSeleccionada = null;
+        primerBotonSeleccionado = null;
+    }
+    private void finalizarJuego() {
+        javafx.scene.control.Alert alerta = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
+        alerta.setTitle("¡Juego Terminado!");
+        alerta.setHeaderText(null);
+        alerta.setContentText("¡Felicidades! Has encontrado todas las parejas.");
+        alerta.showAndWait();
     }
 }
